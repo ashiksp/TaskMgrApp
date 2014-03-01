431a571d5e073607b856a433939a23da1335d183
ashiksp <ashik.sp@tcs.com>
2014-03-02 00:16:02 +0530

    csv format modified

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -48,7 +48,7 @@ def index
 
 	respond_to do |format|
 	format.html
-    format.csv { send_data @tasks.to_csv }
+    format.csv { send_data @tasks.to_csv(col_sep: ",\t") }
     #format.csv { render text: @tasks.to_csv }
     format.xls #{ send_data @tasks.to_csv(col_sep: "\t") }
 end
